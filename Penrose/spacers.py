32e06dbc3efd9b917313cfc609c6a41a57f7fbbc
Jean-Baptiste Rouquier <jrouquie@example.com>
2016-08-21 21:34:19 +0200

    add lozenges, bugfixes (add 1e-6, move line "prep"), speed optimization

--- Penrose/spacers.py
+++ Penrose/spacers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # coding: utf-8
 # The goal of this code is to generate suitable spacers (/croisillons/) to make a physical Penrose tiling in a bathroom.
 # (We can't use the generic cross-shaped spacers...)
@@ -7,8 +8,9 @@
 ################################################################################
 # params
 scale = 100/35.27 # This scales 1 svg unit (pixel) to 1mm
-grout_width = 3 * scale # 3mm (grout is /joint/)
+grout_width = scale * 3 # 3mm (french for grout is /joint/)
 spacer_length = grout_width * 4
+lozenge_fat_width = scale * 50
 paperwidth, paperheight = 297*scale, 420*scale   # A3. «paper» will actually be Plexiglas.
 
 ################################################################################
@@ -26,6 +28,8 @@ import shapely.geometry, shapely.affinity, shapely.prepared
 ################################################################################
 # Build the shapes
 
+####################
+# Spacers
 # .buffer computes the points at distance at most grout_width/2, approximating quarter-circles with 3 points
 branch = shapely.geometry.LineString([(0,0),(spacer_length,0)])\
     .buffer(grout_width/2, resolution=3)
@@ -41,18 +45,34 @@ def spacer(angles):
         branches = branches.union(rotated_branch)
     return branches
 
-# approximated from manual counting on decompte_sommets.xcf
+# approximated from manual counting on a tiling
 # vertex are numbered according to http://images.math.cnrs.fr/IMG/jpg/vertex_atlas.jpg
-proportions =        [27, 70, 17, 11, 44, 9, 12]
-proportions_approx = [ 9, 24,  6,  4, 15, 3,  4]
-spacers = (
-    [spacer([4,4,2])]        *proportions[0]
-  + [spacer([3,3,4])]        *proportions[1]
-  + [spacer([4,2,2,2])]      *proportions[2]
-  + [spacer([2,2,2,2,2])]    *proportions[3]
-  + [spacer([1,2,1,3,3])]    *proportions[4]
-  + [spacer([1,1,2,2,2,2])]  *proportions[5]
-  + [spacer([1,1,2,1,1,2,2])]*proportions[6])
+proportions = [27, 70, 17, 11, 44, 9, 12]
+proportions = [ 9, 23,  6,  4, 15, 3,  4] # approximated from previous list
+def spacers(nb_repetitions=1):
+    return [spacer([4,4,2])]        *proportions[0] * nb_repetitions \
+         + [spacer([3,3,4])]        *proportions[1] * nb_repetitions \
+         + [spacer([4,2,2,2])]      *proportions[2] * nb_repetitions \
+         + [spacer([2,2,2,2,2])]    *proportions[3] * nb_repetitions \
+         + [spacer([1,2,1,3,3])]    *proportions[4] * nb_repetitions \
+         + [spacer([1,1,2,2,2,2])]  *proportions[5] * nb_repetitions \
+         + [spacer([1,1,2,1,1,2,2])]*proportions[6] * nb_repetitions
+
+####################
+# Lozenges
+a = (lozenge_fat_width -grout_width)/2
+band1 = shapely.geometry.Polygon([(-3*a,-a), (-3*a,a), (3*a,a), (3*a,-a)])
+band2 = shapely.affinity.rotate(band1,72,(0,0))
+lozenge_fat = band1.intersection(band2)
+
+phi = (math.sqrt(5) +1)/2
+a = (lozenge_fat_width/phi  -grout_width) /2
+band1 = shapely.geometry.Polygon([(-3*a,-a), (-3*a,a), (3*a,a), (3*a,-a)])   # same as for lozenge_fat
+band2 = shapely.affinity.rotate(band1,36,(0,0))
+lozenge_slim = band1.intersection(band2)
+
+def lozenges(nb_repetitions=1):
+    return [lozenge_fat] * nb_repetitions + [lozenge_slim] * nb_repetitions
 
 ################################################################################
 # Place the shapes on the sheet
@@ -76,7 +96,7 @@ def tetris_pack(geoms, width, stepx, stepy, nb_orientations):
         for i in range(nb_orientations): # try all orientations
             geom_rotated = shapely.affinity.rotate(geom, 360/nb_orientations*i, origin=(0,0))
             minx,miny,maxx,maxy = geom_rotated.bounds
-            for x in range(  int(math.ceil(-minx/stepx)),  int(math.floor((width-maxx)/stepx)) ): # try each column
+            for x in range( int(math.ceil(-minx/stepx)),  int(math.floor((width-maxx)/stepx)) ): # try each column
                 geom_xshifted = shapely.affinity.translate(geom_rotated, x*stepx, yoff=-miny)
                 yoff=starting_yoffs[x] # no need to check lower than that
                 geom_yshifted = geom_xshifted
@@ -84,24 +104,34 @@ def tetris_pack(geoms, width, stepx, stepy, nb_orientations):
                     yoff += stepy
                     geom_yshifted = shapely.affinity.translate(geom_xshifted, xoff=0, yoff=yoff)
                 possible_positions.append({'x':x, 'yoff':yoff, 'maxy':yoff+maxy-miny, 'geom':geom_yshifted})
-        best_position = min(possible_positions, key = lambda d: d['maxy'])
+        best_position = min(possible_positions, key = lambda d: (d['maxy'], d['x']))
         starting_yoffs[best_position['x']] = best_position['yoff']
         result = result.union(best_position['geom'])
         simplified_result = simplified_result.union(best_position['geom'])
-        simplified_result_prepared = shapely.prepared.prep(simplified_result)  # this makes intersection tests more efficient
-        minx,miny,maxx = best_position['geom'].bounds[0:3]
+
+        # trade some compacity for speed: also add to simplified_result all points below miny of the newly placed piece
+        minx,miny,maxx,maxy = best_position['geom'].bounds
         simplified_result = simplified_result.union(
-            shapely.geometry.Polygon([(minx,-1e-6), (minx,miny), (maxx,miny), (maxx,-1e-6)]))
+            shapely.geometry.Polygon([(minx,-1e-6), (minx,miny-1e-6), (maxx,miny-1e-6), (maxx,-1e-6)]))
+        for x in range( int(math.ceil(minx/stepx)),  int(math.floor(maxx/stepx)) ):
+            starting_yoffs[x] = max(starting_yoffs[x], miny)
+
+        simplified_result_prepared = shapely.prepared.prep(simplified_result) # this makes intersection tests more efficient
         nb_placed+=1
         sys.stdout.write("\rPlaced:{}".format(nb_placed)); sys.stdout.flush()
     return result
 
-print ("To place:{}".format(len(spacers)))
-# for debugging:
-# random.shuffle(spacers)
-# packing = tetris_pack(spacers[0:20], paperwidth/3, 3*scale, 3*scale, 10)
-packing = tetris_pack(spacers, paperwidth, 3*scale, 3*scale, 10)
-print()
+####################
+# Here are a few more choices for the user
+to_place = lozenges(nb_repetitions=13) + spacers(nb_repetitions=2)
+
+# quick run for debugging:
+# to_place = spacers(); random.shuffle(to_place); paperwidth = paperwidth/3
+# to_place = to_place[0:70];
+
+print ("To place:{}".format(len(to_place)))
+packing = tetris_pack(to_place, paperwidth, 3*scale, 3*scale, 10)
+sys.stdout.write("\n")
 
 ################################################################################
 # Output the result with cairo
@@ -123,8 +153,11 @@ def render_shapely_to_cairo(geom, context):
     svg = rsvg.Handle(data=svg)
     svg.render_cairo(context)
 
-surface = cairo.PDFSurface("spacers.pdf", paperwidth, paperheight)
-ctx = cairo.Context(surface)
-render_shapely_to_cairo(packing, ctx)
+for surface in [
+        cairo.PDFSurface("spacers.pdf", paperwidth, paperheight),
+        cairo.PSSurface ("spacers.ps",  paperwidth, paperheight),
+        cairo.SVGSurface("spacers.svg", paperwidth, paperheight)]:
+    ctx = cairo.Context(surface)
+    render_shapely_to_cairo(packing, ctx)
 
 ctx.show_page()
